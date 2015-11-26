e0bc2692d42865cb8ee04431c2f81679eb88ee36
Urmish <urmish24@gmail.com>
2015-11-26 14:36:31 -0600

    Model Based System Coding Complete
    Debugging Pending

--- LabFunctions/IncrementalLowRankCompletion.m
+++ LabFunctions/IncrementalLowRankCompletion.m
@@ -8,13 +8,11 @@ m_nextM = i_matrix;
 for i=1:i_iterCount
     [m_U,m_D,m_V] = svd(m_nextM,'econ');
     m_g = diag(m_D);
-    m_h = m_g/m_g(1);
-    m_ind = (m_h>=i_threshold);
-    m_g(~m_ind) = 0;
+    m_g = TopKEigenValues(m_g,i_threshold);
     m_nextM = m_U*diag(m_g)*(m_V');
     m_k = find(i_matrix);
     m_nextM(m_k) = i_matrix(m_k);
-    m_nextM = round(m_nextM);
+    %m_nextM = round(m_nextM);
 end
 o_matrix = m_nextM;
 o_AveragePerCustomer = sum(o_matrix,2)./sum(o_matrix~=0,2);
--- ModelBasedPrediction/ModelBasedSVD.m
+++ ModelBasedPrediction/ModelBasedSVD.m
@@ -1,17 +1,19 @@
 clear;
 close all;
 % Parameter Values
-singularValueThreshold = 0.8;
-predThreshold = 3.5;
+singularValueThreshold = 0.7;
+predThreshold = 3.6;
 customerId = 192;
-modelNumber = 0; %0 - Average Value based %1 - Incremental SVD based
+modelNumber = 1; %0 - Average Value based %1 - Incremental SVD based
 iterCount = 100; %Used by Incremental SVD
+NumNearestNeighbor = 20;
+knnDistanceMeasure = 'cosine';
 
 X = ConvertUDataToMatrix();
 if (modelNumber == 0)
-    [Pred, AveragePerCustomer] = AverageValueBasedMatrixCompletion(X',singularValueThreshold);
+    [Pred, ~] = AverageValueBasedMatrixCompletion(X',singularValueThreshold);
     Pred = Pred';
-    AveragePerCustomer = mean(Pred);
+    AveragePerCustomer = mean(Pred,2);
 else
     [Pred, AveragePerCustomer] = IncrementalLowRankCompletion(X,iterCount,singularValueThreshold);
 end
@@ -20,22 +22,39 @@ end
 index_nonzero = find(X~=0);
 error = norm(X(index_nonzero) - Pred(index_nonzero))/norm(X(index_nonzero));
 %Prediction
-
+fprintf('Prediction Based Recommendation - \n');
 if (predThreshold > AveragePerCustomer(customerId))
     possibleRecommendation = Pred(customerId,:)>predThreshold;
     removeRecommendation = X(customerId,:)~=0;
     possibleRecommendation(removeRecommendation) = 0;
     moviesUnSorted = Pred(customerId,possibleRecommendation);
-    [SortedMovies, Index] = sort(moviesUnSorted);
+    [SortedMovies, Index] = sort(moviesUnSorted,'descend');
     if (size(SortedMovies,2) == 0)
         fprintf('No movies beyond the threshold\n');
     else
         fprintf('Top 5 movies are - ');
-        Index(1,5)
+        Index(1,1:5)
     end
 else
     fprintf('Average Values for this customer have been high and it is difficult to comeup with a prediction using this technique\n');    
 end
 
+fprintf('*********************************\n');
 %Nearest Neighbor
-
+[U S V] = svd(Pred,'econ');
+Sk = TopKEigenValues(diag(S),singularValueThreshold);
+k = sum(Sk~=0);
+Uk = U(:,1:k);
+Sk = diag(Sk);
+Sk = Sk(1:k,1:k);
+NNMatrix = Uk*(Sk.^0.5);
+Mdl = KDTreeSearcher(NNMatrix);
+[n,d] = knnsearch(Mdl,NNMatrix(customerId,:),'k',NumNearestNeighbor+1);
+n = n(1,2:NumNearestNeighbor+1); %As the first column is the customer himself
+HighRatedMoviesOfNeighbors = X(n,:);
+meanRating = mean(HighRatedMoviesOfNeighbors);
+removeRecommendation = X(customerId,:)~=0;
+meanRating(removeRecommendation) = 0;
+[SortedMovies, Index] = sort(meanRating,'descend');
+fprintf('Recommendation Based on Nearest Neighbor Search - \n');
+Index(1,1:5)
