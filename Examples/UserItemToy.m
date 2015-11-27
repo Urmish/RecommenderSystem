a60cdb20c419f2bd85521e3d7e5ed56d7103651d
lokeshjindal15 <lokeshjindal15@gmail.com>
2015-11-26 22:40:09 -0600

    Adding function to calculate PC similarity of a vector with each row of a Matrix. Ading a toy example to show difference between cosine similarity and PC similarity

---
+++ Examples/UserItemToy.m
@@ -0,0 +1,26 @@
+% Example to illustrate user-based and item-based similarity and
+% recommendation
+
+%           Matrix      Titanic     DieHard    ForrestGump     Wall-E
+% John        5             1                       2             2
+% Lucy        1             5           2           5             5
+% Eric        2                         3           5             4
+% Diane       4             3           5           3   
+
+X = [5 1 0 2 2; 1 5 2 5 5; 2 0 3 5 4; 4 3 5 3 0];
+
+% Want to generate rating for movie Titanic for Eric
+% Find similarity of Eric with all others
+E_Cossim = CosSimVecMatrix((X(3,:))', X)
+E_PCsim = PCSimVecMatrix((X(3,:))', X)
+
+% Cossim highest for Lucy 0.8064
+% Cossim 2nd highest for Diane 0.6732
+
+% E_PCsim highest for Lucy 0.9218
+% E_PCsim 2nd highest for Diane -0.6592 Contrast
+
+% Diane's mean is 3.75 Eric's mean is 3.5
+% If you look at their normalized vectors
+% Eric's ->     -1.5000         0   -0.5000    1.5000    0.5000
+% Dian'es ->    0.2500   -0.7500    1.2500   -0.7500         0
---
+++ LabFunctions/PCSimVecMatrix.m
@@ -0,0 +1,29 @@
+function o_SimilarityVec  = PCSimVecMatrix( i_Vec1, i_M)                                                           
+% Calculates Person Coefficient similarity between a vector and rows of
+% matrix M
+% Completion
+%   i_Vec1 - first input vector
+%   i_M - second input vector
+%   o_SimilarityVecM - output the vector of similarities
+    meanVec = sum(i_Vec1)/nnz(i_Vec1); % mean of all ratings excluding unrated items
+    meanMrows = sum(i_M,2)./sum(i_M~=0,2)
+    
+    nonzeroM = i_M~= 0; % matrix of 1s for rated entries
+    nonzeroVec = i_Vec1 ~= 0;
+    normVec1 = i_Vec1 - meanVec*nonzeroVec;
+    [m,n] = size(i_M);
+    normM = i_M - repmat(meanMrows, 1, n).* nonzeroM
+    o_SimilarityVec = -99*ones(m,1);
+    for i=1:m
+        numerator = normM(i,:)*normVec1;
+        
+        vecindices = zeros(1,n);
+        rowindices = zeros(1,n);
+        vecindices(i_Vec1' ~= 0)  = 1;
+        rowindices(i_M(i,:) ~= 0) = 1;
+        finalindices = vecindices .* rowindices;% indices for items rated by both
+        % denominator accounts for variance of only items rated by both
+        denominator = norm(normVec1'.*finalindices,2) * norm(normM(i,:).*finalindices,2);
+        o_SimilarityVec(i,1) = numerator/denominator;
+    end
+ end
