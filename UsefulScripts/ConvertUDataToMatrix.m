cde26b629345d442b27c111a3d25c579ab1029b2
Urmish Thakker <uthakker@macaroni-04.cs.wisc.edu>
2015-11-28 23:10:56 -0600

    Made ConvertUDataToMatrix more general

--- UsefulScripts/ConvertUDataToMatrix.m
+++ UsefulScripts/ConvertUDataToMatrix.m
@@ -1,10 +1,11 @@
-function [ X ] = ConvertUDataToMatrix()
-%CONVERTUDATATOMATRIX - Converts u.data to an mxn matrix where m is the
+function [ X ] = ConvertUDataToMatrix(i_filename)
+%CONVERTUDATATOMATRIX - Converts i_filename to an mxn matrix where m is the
 %number of users and n is the number of movies
-fid = fopen('Data/u.data','r');
+% eg i_filename - 'Data/u.data'
+fid = fopen(i_filename,'r');
 InputText=textscan(fid,'%d\t%d\t%d\t%d');
-m = length(unique(InputText{1})); %m is the number of users, InputText{1} is the list of users
-n = length(unique(InputText{2})); %n is the number of movies, InputText{2} is the list of movies rated
+m = max(unique(InputText{1})); %m is the number of users, InputText{1} is the list of users
+n = max(unique(InputText{2})); %n is the number of movies, InputText{2} is the list of movies rated
 X = zeros(m,n);
 for i=1:length(InputText{1})
     X(InputText{1}(i),InputText{2}(i)) = InputText{3}(i);
