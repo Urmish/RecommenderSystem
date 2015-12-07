c0aa65695ac3408f0ccccc254d9f699649078e01
Urmish Thakker <uthakker@sally.cs.wisc.edu>
2015-12-07 14:06:23 -0600

    Added Example, modified Document

--- Examples/ToyExampleAverageValueSVD.m
+++ Examples/ToyExampleAverageValueSVD.m
@@ -9,5 +9,5 @@ singularValueThreshold = 0.5;
 iterationCount = 1000;
 X = [5 1 0 2 2; 1 5 2 5 5; 2 0 3 5 4; 4 3 5 3 0];
 
-%Incremental Low Rank Completion
-[completion2 ~] = IncrementalLowRankCompletion(X,iterationCount,singularValueThreshold);
+%Average Value Based Completion
+[completion1 ~] = AverageValueBasedMatrixCompletion(X,singularValueThreshold); %To verify this is working, change the singular value threshold to value 0
